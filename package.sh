38e59c2cba90ba3ba992761997a629a4295246c1
Bernhard Posselt <bernhard@posselt.at>
2023-03-02 00:21:48 +0100

    fix build

--- package.sh
+++ package.sh
@@ -14,7 +14,7 @@ mkdir -p build/pf2e-kingmaker-map-remake/
 # create archive
 node ./scripts/update-module-json.mjs "$version"
 cp module.json README.md LICENSE CHANGELOG.md OpenGameLicense.md build/pf2e-kingmaker-map-remake/
-cp -r packs/ build/pf2e-kingmaker-map-remake/
+cp -r packs/ assets/ build/pf2e-kingmaker-map-remake/
 
 cd build
 zip -r release.zip pf2e-kingmaker-map-remake
